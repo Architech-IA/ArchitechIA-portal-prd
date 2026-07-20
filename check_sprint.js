3e52057066adabc8248513be114b4c3d998aaf3d
Freddy Orozco <freddy-orozco29@hotmail.com>
2026-07-20 06:04:14 +0000

    chore: remove temp backfill scripts

--- check_sprint.js
+++
@@ -1,3 +0,0 @@
-const { PrismaClient } = require("@prisma/client")
-const p = new PrismaClient()
-p.sprint.findMany({ include: { items: true } }).then(r => { console.log(JSON.stringify(r, null, 2)); p.$disconnect() })
